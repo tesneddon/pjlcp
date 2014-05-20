f2860a6d6b422c91aaae1f8f7e28d8ce9e782c1e
Tim E. Sneddon <tim@sneddon.id.au>
2014-05-20 12:45:53 +0800

    Was not setting new flag to indicate we need to listen for a response.

--- src/status.c
+++ src/status.c
@@ -64,7 +64,7 @@ int act_echo(void *ctx) {
         break;
 
     case OP_FINISH:
-        pcbp->flags2.send_pjl = 1;
+        pcbp->flags2.send_pjl = pcbp->flags2.expect_ack = 1;
         break;
 
     default:
@@ -93,7 +93,7 @@ int act_info(void *ctx) {
         if (pcbp->pjlbuf == 0) {
             pcbp->pjlbuf = cat(pcbp->pjlbuf, "INFO ID");
         }
-        pcbp->flags2.send_pjl = 1;
+        pcbp->flags2.send_pjl = pcbp->flags2.expect_ack = 1;
         break;
 
     default:
